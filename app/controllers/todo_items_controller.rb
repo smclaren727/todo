1fc5a94f88061782f7f6df01ff9c6189de63d6d0
smclaren727 <smclaren727@gmail.com>
2015-09-01 20:23:43 -0400

    Completed styling and views

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -3,8 +3,8 @@ class TodoItemsController < ApplicationController
 	before_action :set_todo_item, except: [:create]
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.create(todo_item_params)
+    redirect_to @todo_list
   end
 
   def destroy
